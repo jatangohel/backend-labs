693b2baa87b2d31f60bd69d38b45d29558f36afb
greeshmanimmala <greeshmareddy909@gmail.com>
2020-06-28 19:26:30 -0400

    Adding second lab file of beginner module

---
+++ 01 Beginner/lab02.php
@@ -0,0 +1,21 @@
+<?php
+$temperature=-30;
+
+switch($temperature){
+
+case '-10':
+    echo "The weather is not so cool";
+break;
+case '-20':
+    echo "The weather is moderate";
+break;
+case '-5':
+    echo "The weather is breezy";
+break;
+case '-30':
+    echo "The weather is so cool cover yourself";
+break;
+default:
+echo "You are free from winter";
+}
+?>
